878f2dd3d0cb2d4ce583acf0e939f6cf209751c0
robin <robinganemccalla@gmail.com>
2019-01-08 21:39:38 -0500

    added evaluatePredictions function to Contract, wrote a test for it which currently doesn't pass

--- contracts/Mining.sol
+++ contracts/Mining.sol
@@ -12,8 +12,9 @@ contract Mining{
 		Prediction[] predictions;
 		uint length;
 	}
-	mapping(address => UserPredictions) predictionAddress;
-	Prediction[] predictionArray;
+	mapping(address => UserPredictions) public predictionAddress;
+	mapping(uint => UserPredictions) public predictionsAtTime;
+	Prediction[] public predictionArray;
 	function getDifficulty() public returns(uint){
 		return block.difficulty;
 	}
@@ -22,6 +23,8 @@ contract Mining{
 		predictionArray.push(prediction);
 		predictionAddress[msg.sender].predictions.push(prediction);
 		predictionAddress[msg.sender].length++;
+		predictionsAtTime[date].predictions.push(prediction);
+		predictionsAtTime[date].length++;
 	}
 	function getPredictionFromAddress() internal returns (UserPredictions memory){
 		return predictionAddress[msg.sender];
@@ -30,5 +33,40 @@ contract Mining{
 		UserPredictions memory predictionArray = getPredictionFromAddress();
 		Prediction memory prediction = predictionArray.predictions[index];
 		return (prediction.predicter,prediction.amount,prediction.difficultyPrediction,prediction.above,prediction.time);
+	}
+	event Refund(address refundAddress, uint amount);
+	function evaluatePredictions(uint time, uint difficulty) public{
+		
+		//update to find correct prediction at time
+		UserPredictions storage predictionsToCheck = predictionsAtTime[time];
+		//require(time == predictionToCheck.time);
+
+		uint incorrectSum;
+		uint correctSum;
+		address[] storage correctAddresses;
+		uint correctWagers;
+		uint[] storage correctWagersValues;
+		for (uint i = 0; i < predictionsToCheck.length; i++){
+			//this can be changed to block.difficults in other context, like when not testing
+			if (difficulty == predictionsToCheck.predictions[i].difficultyPrediction){
+				address refunder = predictionsToCheck.predictions[i].predicter;
+				refunder.transfer(predictionsToCheck.predictions[i].amount);
+				Refund(refunder, predictionsToCheck.predictions[i].amount);
+			}
+			else if (((block.difficulty >= predictionsToCheck.predictions[i].difficultyPrediction) && predictionsToCheck.predictions[i].above == true) || 
+		((block.difficulty <= predictionsToCheck.predictions[i].difficultyPrediction) && predictionsToCheck.predictions[i].above == false)) {
+				correctSum = correctSum + predictionsToCheck.predictions[i].amount;
+				correctAddresses.push(predictionsToCheck.predictions[i].predicter);
+				correctWagersValues.push(predictionsToCheck.predictions[i].amount);
+				correctWagers++;
+			}
+			else {
+				incorrectSum = incorrectSum + predictionsToCheck.predictions[i].amount;
+			}
+		}
+		for(uint j=0; j < correctWagers; j++){
+			uint winningValue = incorrectSum*(correctWagersValues[j]/incorrectSum+correctSum);
+			correctAddresses[j].send(winningValue);
+		}
 	}
 }
--- test/MiningTest.js
+++ test/MiningTest.js
@@ -9,41 +9,52 @@ contract("Mining", accounts => {
 	})
 	it("set Prediction should create a prediction", async () => {
 		const myMining = await Mining.deployed();
-		await myMining.setPrediction(1546885009,0,false , {from: accounts[0], value: 20000000});
+		await myMining.setPrediction(1546885009,0,false , {from: accounts[0], value: 2000000});
 		let prediction =  await myMining.getPredictionFromAddressExternal.call(0,accounts[0]);
 		assert.equal(prediction[0], accounts[0], "address is not set correctly");
-		assert.equal(prediction[1].toNumber(), 20000000, "amount is not set correctly");
+		assert.equal(prediction[1].toNumber(), 2000000, "amount is not set correctly");
 		assert.equal(prediction[2], 0, "difficulty is not set correctly");
 		assert.equal(prediction[3], false, "direction is not set correctly");
 		assert.equal(prediction[4], 1546885009, "time is not set correctly");
 	})
 	it("prediction should be added to array", async() => {
 		const myMining = await Mining.deployed();
-		await myMining.setPrediction(1546885009,0,false , {from: accounts[0], value: 20000000});
 		let predictionArray = await myMining.predictionArray.call(0);
-		//let prediction = predictions.predictions[0];
 		assert.equal(predictionArray[0], accounts[0], "address is not set correctly");
-		assert.equal(predictionArray[1].toNumber(), 20000000, "amount is not set correctly");
+		assert.equal(predictionArray[1].toNumber(), 2000000, "amount is not set correctly");
 		assert.equal(predictionArray[2], 0, "difficulty is not set correctly");
 		assert.equal(predictionArray[3], false, "direction is not set correctly");
 		assert.equal(predictionArray[4], 1546885009, "time is not set correctly");
 	})
 	it("account should have less ether", async() => {
 		const myMining = await Mining.deployed();
-		await myMining.setPrediction(1546885009,0,false , {from: accounts[0], value: 20000000});
-		let account0Balance, account1Balance,contractBalance;
+		let account0Balance, account1Balance;
 		account0balance = await web3.eth.getBalance(accounts[0], function(err,res) {
             });
 		account1balance = await web3.eth.getBalance(accounts[1], function(err,res) {
             });
-		contractBalance = await web3.eth.getBalance(myMining.address, function(err,res) {
-            });
 		account0balance = parseInt(web3.utils.fromWei(account0balance, 'ether'));
 		account1balance = parseInt(web3.utils.fromWei(account1balance, 'ether'));
-		contractBalance = parseInt(web3.utils.fromWei(contractBalance, 'ether'));
-		var balance = await web3.eth.getBalance(myMining.address);
+		var contractBalance = await web3.eth.getBalance(myMining.address);
 		assert.isAbove(account1balance,account0balance,"account 0 has not had ether taken from it");
-		assert.isAbove(parseInt(balance), 0, "contract has not had ether transfered to it");
+		assert.isAbove(parseInt(contractBalance), 0, "contract has not had ether transfered to it");
+	})
+	it("prediction should return ether when difficulty guess is exactly right", async() => {
+		const myMining = await Mining.deployed();
+		await myMining.evaluatePredictions(1546885009,0);
+		account0balance = await web3.eth.getBalance(accounts[0], function(err,res) {
+            });
+		account1balance = await web3.eth.getBalance(accounts[1], function(err,res) {
+            });
+
+		account0balance = parseInt(web3.utils.fromWei(account0balance, 'ether'));
+		account1balance = parseInt(web3.utils.fromWei(account1balance, 'ether'));
+		console.log(account0balance);
+		console.log(account1balance);
+		console.log(accounts[0]);
+
+		assert.equal(account1balance,account0balance,"balances are not equal!");
+
 	})
 })
 
