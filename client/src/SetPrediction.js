38dab7f641a1b197b4025dd8465d45fc5aeb2989
robin <robinganemccalla@gmail.com>
2019-01-10 17:13:55 -0500

    have data being submitted from react (must test)

--- client/src/SetPrediction.js
+++ client/src/SetPrediction.js
@@ -6,6 +6,8 @@ class SetPrediction extends React.Component {
 	    this.handleSubmit = this.handleSubmit.bind(this);
 	   	this.state = { date:'', difficulty:0, above:false, ether:0 };
 
+
+
   }
 
 	//this could be set to autopopulate current date or date in near future and current difficulty
@@ -18,27 +20,17 @@ class SetPrediction extends React.Component {
         })
     }
 
-	handleSubmit(e) {
-		const formData = {};
-		e.preventDefault();
-		    for (const field in this.refs) {
-		    	if ((typeof field) == Date){
-		    		formData[field] = this.refs[field].value.getTime() / 1000;
-		    	}
-		    	else{
-      				formData[field] = this.refs[field].value;
-				}
-			}
-    console.log('-->', formData);
-
-	}
+handleSubmit() {
+	var con = this.props.drizzle.contracts.Mining
+	con.methods.setPrediction.cacheSend(this.state.date,this.state.difficulty,this.state.above);
+}
 	/*handlepredictionDateChange: function(e) {
 		this.setState({date: e.target.value});
 	},
 	handlepredictionDifficulty: function(e) {
 		this.setState({difficulty: e.target.value});
 	},*/
-	ComponentWillMount(){
+	ComponentDidMount(){
 	    const { drizzle } = this.props;
 	    const contract = drizzle.contracts.Mining;
 
@@ -48,7 +40,6 @@ class SetPrediction extends React.Component {
 		return(
 		<div id='predictionInput'>
 		<h2>Make Prediction</h2>
-		<form onSubmit={this.handleSubmit}>
 		<label>Date</label>
 		<input type='datetime-local' name='date' value={this.state.date} onChange={e =>this.handleChange(e)}></input>
 		<br/>
@@ -63,8 +54,7 @@ class SetPrediction extends React.Component {
 		<br/>
 		<label>Ether</label>
 		<input type='number' value={this.state.predictionEther} onChange={e =>this.handleChange(e)}></input>
-		<input type='submit'></input>
-		</form>
+		<button onClick={this.handleSubmit}>Submit Prediction</button>
 		</div>)
 	}
 }
